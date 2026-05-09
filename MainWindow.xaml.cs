2c215b25ce6da4fc2e62ade21f295a89d5f300e0
KangDongJoon <ehdwns1119@naver.com>
2026-05-10 08:40:33 +0900

    초기 커밋.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Text;
+using System.Windows;
+
+namespace MiniForensicAnalyzer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+
+            if (dialog.ShowDialog() == true)
+            {
+                FilePathText.Text = dialog.FileName;
+
+                byte[] bytes = File.ReadAllBytes(dialog.FileName);
+
+                StringBuilder hex = new StringBuilder();
+
+                foreach (byte b in bytes)
+                {
+                    hex.Append(b);
+                    hex.Append(' ');
+                }
+
+                HexViewr.Text = hex.ToString();
+            }
+        }
+    }
+}
