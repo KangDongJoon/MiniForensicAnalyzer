ea1dae07110eeabd63e38b3dacfdc081ebd37d56
KangDongJoon <ehdwns1119@naver.com>
2026-05-10 16:14:15 +0900

    [ADD] - 파일 선택 기능 추가 - 파일에서 byte값 추출 - 추출된 byte값을 sha256을 이용해 hash

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Text;
 using System.Windows;
+using System.Security.Cryptography;
 
 namespace MiniForensicAnalyzer
 {
@@ -15,15 +16,17 @@ namespace MiniForensicAnalyzer
             InitializeComponent();
         }
 
+        private string currentFilePath;
         private void OpenFile_Click(object sender, RoutedEventArgs e)
         {
             var dialog = new OpenFileDialog();
 
             if (dialog.ShowDialog() == true)
             {
-                FilePathText.Text = dialog.FileName;
+                currentFilePath = dialog.FileName;
+                FilePathText.Text = currentFilePath;
 
-                byte[] bytes = File.ReadAllBytes(dialog.FileName);
+                byte[] bytes = File.ReadAllBytes(currentFilePath);
 
                 StringBuilder hex = new StringBuilder();
 
@@ -34,6 +37,32 @@ namespace MiniForensicAnalyzer
                 }
 
                 HexViewr.Text = hex.ToString();
+            }
+        }
+
+        private void HashButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(currentFilePath))
+            {
+                MessageBox.Show("파일을 먼저 선택하세요");
+                return;
+            }
+
+            byte[] fileBytes = File.ReadAllBytes(currentFilePath);
+
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hashBytes = sha256.ComputeHash(fileBytes);
+
+                StringBuilder sb = new StringBuilder();
+
+                foreach (byte b in hashBytes)
+                {
+                    sb.Append(b.ToString("X2"));
+
+                }
+
+                HashText.Text = $"SHA256: {sb}";
             }
         }
     }
